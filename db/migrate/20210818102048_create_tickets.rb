452f84aca797b37e4a63c29a2aab28b3da743995
Jackie Sun <jackiesun2@gmail.com>
2021-08-18 20:26:44 +1000

    initial commit

---
+++ db/migrate/20210818102048_create_tickets.rb
@@ -0,0 +1,11 @@
+class CreateTickets < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tickets do |t|
+      t.string :title
+      t.text :description
+      t.date :date
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/tickets.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :ticket do
+    title { "MyString" }
+    description { "MyText" }
+    date { "2021-08-18" }
+  end
+end
